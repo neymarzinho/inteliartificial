04646faa48e56777f9500be0f45123013cc825a1
neymarzinho <128197097+neymarzinho@users.noreply.github.com>
2024-08-23 22:25:03 -0300

    .

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Isso é incrivel!",
                 afirmacao: "afirmação"
             },
             {
@@ -26,7 +26,7 @@ const perguntas = [
                 afirmacao: "afirmação"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet.",
                 afirmacao: "afirmação"
             }
         ]
